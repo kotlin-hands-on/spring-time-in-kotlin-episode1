cec18c0c8a9f87898973c2db6339249760e282c3
Anton Arhipov <ant.arhipov@gmail.com>
2020-12-21 09:40:12 +0200

    Initial commit

---
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -0,0 +1,52 @@
+package demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageResource(val service: MessageService) {
+    @GetMapping
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
+
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessages(): List<Message> = db.findMessages()
+
+    fun post(message: Message){
+        db.save(message)
+    }
+}
+
+interface MessageRepository : CrudRepository<Message, String>{
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
+}
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
+
